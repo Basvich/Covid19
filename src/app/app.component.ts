22b2331ca4f3e793497e793173ca7cf1219d36d0
Basilio <basilio.alvarez@gmail.com>
2020-03-24 00:04:21 +0100

    edit options

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
-import { IHumanOpt, Human, HumanFactory, IInfectionOptions, HStatus } from './su-vir/ihuman';
-import { Rectangle, Point, IDataPoint, KdTree } from './su-vir/kd-tree';
+import {IHumanOpt, Human, HumanFactory, IInfectionOptions, HStatus, getSuccess} from './su-vir/ihuman';
+import {Rectangle, Point, IDataPoint, KdTree, INodeKdTree, IPosition} from './su-vir/kd-tree';
 import * as p5 from 'p5';
 //import {Chart} from 'chart.js';
 import * as Chart from 'chart.js';
@@ -16,40 +16,52 @@ interface IHumanPoint extends IDataPoint {
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  readonly populationNumber=4000;
+  readonly populationNumber = 16000;
   private canvasP5: p5;
   title = 'covid19';
   humans: IHumanPoint[];
   baseKd: KdTree;
   currentDay = 0;
   mediumDistance: number;
+  sqrDistanceBase: number;
 
   public chart: Chart = null;
   /** Elementos infecciosos */
   public infecciosos = 0;
+  /** infectados y no curados o muertos */
+  public infectados = 0;
+  /** recuperados vivos */
+  public recuperadosVivos = 0;
 
   public infectOp: IInfectionOptions = {
     distanceBase: 4,
-    contagiousProb: 0.5,
-    humanData:{
-      incubation:{mean:3, stdDev:1.0},
+    contagiousProb: 1,
+    humanData: {
+      incubation: {mean: 3, stdDev: 1.0},
       sintomatic: 0.5,
-      infectionPeriod:{mean:10, stdDev:3}
+      infectionPeriod: {mean: 10, stdDev: 3},
+      lethality: 0.03
     }
   };
 
   public setupHumans() {
+    const inf = (n: INodeKdTree) => {
+      (n.location.data as Human).infect(0, this.infectOp.humanData);
+    };
     const opt: IHumanOpt = {
       zone: new Rectangle(0, 800, 0, 400)
     };
-    const hs = HumanFactory.create(this.populationNumber, opt); // HumanFactory.createTest(); // 
+    this.currentDay = 0;
+    this.sqrDistanceBase = this.infectOp.distanceBase ** 2;
+    this.mediumDistance = Math.sqrt(opt.zone.area() / this.populationNumber);
+    const hs = HumanFactory.create(this.populationNumber, opt); // HumanFactory.createTest(); 
     this.humans = hs.map((h) => ({data: h, point: h.position}));
     this.baseKd = KdTree.createFrom(this.humans);
     // this.baseKd.trace();
-    const all = this.baseKd.GetAll();
     const r = this.baseKd.root;
-    const h0 = r.location.data as Human;
-    h0.infect(0, this.infectOp.humanData);
+    inf(r);
+    inf(r.left);
+    inf(r.right);
     this.canvasP5.redraw();
   }
 
@@ -61,8 +73,8 @@ export class AppComponent implements OnInit {
     this.data2Chart();
   }
 
-  public multStep(nDays){
-    for(let i=0; i< nDays; i++) this.oneStep();
+  public multStep(nDays) {
+    for (let i = 0; i < nDays; i++) this.oneStep();
   }
 
 
@@ -93,12 +105,14 @@ export class AppComponent implements OnInit {
   }
 
   protected colorFromHumanStatus(h: Human) {
-    if(!h.hstatus) return [127,127,127,255];
-    if(h.hstatus & HStatus.infected){
-      if(h.hstatus & HStatus.infectious) return [255, 0, 0, 255];
-      return [255,153,51,255];
+    if (!h.hstatus) return [127, 127, 127, 255];
+    if (h.hstatus & HStatus.infected) {
+      if (h.hstatus & HStatus.infectious) return [255, 0, 0, 255];
+      return [255, 153, 51, 255];
     }
-    if(h.hstatus===HStatus.inmune) return [0,255,0,255];
+    if (h.hstatus === HStatus.inmune) return [0, 255, 0, 255];
+    if (h.hstatus === HStatus.death) return [10, 10, 10, 255]
+
     return [0, 255, 0, 255];
   }
 
@@ -123,12 +137,10 @@ export class AppComponent implements OnInit {
   /** Se realiza la propagación */
   protected propagation() {
     let newInfected: Human[] = null;
-    let totinfectious = 0;
     // Se recorren todos los elementos, mirando los que son infecciosos
     this.humans.forEach(element => {
-      if(!element.data.hstatus) return;
+      if (!element.data.hstatus) return;
       if (element.data.hstatus & HStatus.infectious) {
-        totinfectious++;
         const prov = this.checkInfection(element);
         if (prov && prov.length > 0) {
           if (!newInfected) {
@@ -142,30 +154,63 @@ export class AppComponent implements OnInit {
     if (newInfected && newInfected.length > 0) {
       newInfected.forEach((ni) => ni.infect(this.currentDay, this.infectOp.humanData));
     }
-    this.infecciosos = totinfectious;
   }
 
   /** Se actualiza el estado de salud de cada individuo */
   protected checkHealth() {
-     for(const human of this.humans){
-       human.data.checkStatus(this.currentDay, this.infectOp.humanData);
-     }
+    let totalInfectados = 0;
+    let totalInfecciosos = 0;
+    let totalInmunes = 0;
+    let totalSimptomatic = 0;
+    let totalDeath = 0;
+    for (const human of this.humans) {
+      human.data.checkStatus(this.currentDay, this.infectOp.humanData);
+      if (human.data.hstatus & HStatus.infected) {
+        totalInfectados++;
+        if (human.data.hstatus & HStatus.infectious) {
+          totalInfecciosos++;
+        }
+        if (human.data.hstatus & HStatus.symptomatic) totalSimptomatic++;
+        continue;
+      }
+      if (human.data.hstatus & HStatus.inmune) {
+        totalInmunes++;
+        continue;
+      }
+      if (human.data.hstatus & HStatus.death) totalDeath++;
+    }
+    this.infecciosos = totalInfecciosos;
   }
 
   /** Obtiene la lista de nuevos infectados */
   protected checkInfection(human: IHumanPoint): Human[] {
     let res: Human[] = null;
-    const maxDist = this.infectOp.distanceBase * this.infectOp.distanceBase;
-    const zone: Rectangle = Rectangle.fromRadius(human.point, maxDist); // new Rectangle(1,6,5,10);//
+    const maxDist = 4 * this.infectOp.distanceBase;// Fuera de aquí
+    const zone: Rectangle = Rectangle.fromRadius(human.point, maxDist);
     const candidates = this.baseKd.getInZone(zone);
     if (!candidates || candidates.length === 0) return res;
     // Recorremos los candidatos a ver quien es infectable
     candidates.forEach((c) => {
       const h: Human = c.data as Human;
-      if (!res) res = [];
-      res.push(h);
-    });
+      const dist = this.squareDist(h.position as IPosition, human.point);
+      let prob: number;
+      if (dist < this.sqrDistanceBase) {
+        prob = 1;
+      } else {
+        prob = this.infectOp.contagiousProb / dist;
+      }
+      const contagiado = getSuccess(prob);
+      if (contagiado) {
+        if (!res) res = [];
+        res.push(h);
+      }
+    }
+    );
     return res;
+  }
+
+  protected squareDist(p1: IPosition, p2: IPosition): number {
+    return (p2.x - p1.x) ** 2 + (p2.y - p1.y);
   }
 
   protected initChart() {
@@ -200,7 +245,7 @@ export class AppComponent implements OnInit {
           ]
         },
         showLines: true
-      }    
+      }
     });
   }
 
--- src/app/su-vir/ihuman.ts
+++ src/app/su-vir/ihuman.ts
@@ -1,21 +1,23 @@
-import { IPosition, Rectangle, Point } from './kd-tree';
+import {IPosition, Rectangle, Point} from './kd-tree';
 
 
-export interface INormalDist{
+export interface INormalDist {
   mean: number;
   stdDev: number;
 }
 
-export interface IInfectionHuman{
+export interface IInfectionHuman {
   /** duracion del periodo de incubación */
   incubation: INormalDist;
   /** Probabilidad de tener sintomas */
   sintomatic: number;
   /** Duracion desde la infección hasta la curación */
   infectionPeriod: INormalDist;
+  /** Posibilidad de muerte */
+  lethality: number;
 }
 
-export interface IInfectionOptions{
+export interface IInfectionOptions {
   /** distancia minima a partir de la cual el contagio es directamente la probabilidad */
   distanceBase: number;
   /** Probabilidad de contagio a la minima distancia o inferior */
@@ -23,52 +25,58 @@ export interface IInfectionOptions{
   humanData: IInfectionHuman;
 }
 
-export enum HStatus{
-  none= 0,
-  infected = 1<< 0,
-  incubation = 1<<1,
-  infectious = 1<<2,
-  symptomatic = 1<<3,
-  death = 1<<4,
-  inmune = 1<<5
+export enum HStatus {
+  none = 0,
+  infected = 1 << 0,
+  incubation = 1 << 1,
+  infectious = 1 << 2,
+  symptomatic = 1 << 3,
+  death = 1 << 4,
+  inmune = 1 << 5
 }
 
 
 export class Human {
   position: Point;
-  private nextDay=10000; 
-  hstatus: HStatus=HStatus.none;
+  private nextDay = 10000;
+  hstatus: HStatus = HStatus.none;
   endIncubationDay: number;
   endInfectionDay: number;
+  willDie: boolean;
 
-  public constructor(){
-    
+  public constructor() {
+
   }
 
-  public infect(currentDay: number, opt: IInfectionHuman ){
-    if(this.hstatus) return;
-    this.hstatus=HStatus.infected | HStatus.incubation;
-    this.endIncubationDay=currentDay+getRndNormalDist(opt.incubation);
-    this.endInfectionDay=currentDay+getRndNormalDist(opt.infectionPeriod);
+  public infect(currentDay: number, opt: IInfectionHuman) {
+    if (this.hstatus) return;
+    this.hstatus = HStatus.infected | HStatus.incubation;
+    this.endIncubationDay = currentDay + getRndNormalDist(opt.incubation);
+    this.endInfectionDay = currentDay + getRndNormalDist(opt.infectionPeriod);
+    this.willDie = getSuccess(opt.lethality);
   }
 
-
-  public checkStatus(currentDay: number, opt: IInfectionHuman){
-   if(!this.hstatus) return;
-   if((this.hstatus & HStatus.inmune) || (this.hstatus & HStatus.death)) return;
-   if(this.hstatus & HStatus.infected){
-     if(this.hstatus & HStatus.incubation){
-       if(currentDay>=this.endIncubationDay){
-         this.hstatus &= ~HStatus.incubation;
-         this.hstatus |= HStatus.infectious;
-       }
-       return;
-     } else{
-       if(currentDay>=this.endInfectionDay){
-         this.hstatus=HStatus.inmune;
-       }
-     }
-   }
+  /** Cada persona cambia su estado de salud */
+  public checkStatus(currentDay: number, opt: IInfectionHuman) {
+    if (!this.hstatus) return;
+    if ((this.hstatus & HStatus.inmune) || (this.hstatus & HStatus.death)) return;
+    if (this.hstatus & HStatus.infected) {
+      if (this.hstatus & HStatus.incubation) {
+        if (currentDay >= this.endIncubationDay) {
+          this.hstatus &= ~HStatus.incubation;
+          this.hstatus |= HStatus.infectious;
+        }
+        return;
+      } else {
+        if (currentDay >= this.endInfectionDay) {
+          if (this.willDie) {
+            this.hstatus = HStatus.death;
+          } else {
+            this.hstatus = HStatus.inmune;
+          }
+        }
+      }
+    }
   }
 
 }
@@ -82,7 +90,7 @@ export class HumanFactory {
     const res: Array<Human> = [];
     for (let i = 0; i < count; i++) {
       const nh = new Human();
-      nh.position=getRndPos(opt.zone) ;
+      nh.position = getRndPos(opt.zone);
       res.push(nh);
     }
     return res;
@@ -90,10 +98,10 @@ export class HumanFactory {
 
   public static createTest(): Array<Human> {
     const res: Array<Human> = [];
-    for(let x=0; x<8;x++){
-      for(let y=0; y<8;y++){
+    for (let x = 0; x < 8; x++) {
+      for (let y = 0; y < 8; y++) {
         const nh = new Human();
-        nh.position=new Point(x*4, y*4);
+        nh.position = new Point(x * 4, y * 4);
         res.push(nh);
       }
     }
@@ -101,14 +109,14 @@ export class HumanFactory {
   }
 
   public static createTest2(): Array<Human> {
-    const p: Point[]=[new Point(2,8), new Point(4,7), new Point(2,1),
-      new Point(5,2), new Point(6,4), new Point(8,6),
-      new Point(9,3), new Point(9,4)
+    const p: Point[] = [new Point(2, 8), new Point(4, 7), new Point(2, 1),
+    new Point(5, 2), new Point(6, 4), new Point(8, 6),
+    new Point(9, 3), new Point(9, 4)
     ];
     const res: Array<Human> = [];
-    p.forEach((el)=>{
+    p.forEach((el) => {
       const nh = new Human();
-      nh.position=el;
+      nh.position = el;
       res.push(nh);
     });
     return res;
@@ -117,8 +125,8 @@ export class HumanFactory {
 
 
 /** Si ocurre un suceso con probabilidad indicada (0,1] */
-function getSuccess(prob: number): boolean{
-  return Math.random()<prob;
+export function getSuccess(prob: number): boolean {
+  return Math.random() < prob;
 }
 
 function getRandom(min, max): number {
@@ -129,18 +137,18 @@ function getRandomInt(min, max): number {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function getRndPos(rec: Rectangle): Point{
-  return new Point(getRandom(rec.left,rec.right), getRandom(rec.bottom, rec.top));
+function getRndPos(rec: Rectangle): Point {
+  return new Point(getRandom(rec.left, rec.right), getRandom(rec.bottom, rec.top));
 }
 
-function getRndNormalDist(dat: INormalDist): number{
+function getRndNormalDist(dat: INormalDist): number {
   return getRndNormal(dat.mean, dat.stdDev);
 }
 
 /** Devuelve un dato con dsitrubción normal, usando  Box-Muller aproximación */
-function getRndNormal(mean: number,stdDev: number): number{
-   const u1=1.0 - Math.random();
-   const u2=1.0 - Math.random();
-   const randStdNormal=Math.sqrt(-2.0 * Math.log(u1))*Math.sign(2.0 * Math.PI * u2);
-   return mean+stdDev*randStdNormal;
+function getRndNormal(mean: number, stdDev: number): number {
+  const u1 = 1.0 - Math.random();
+  const u2 = 1.0 - Math.random();
+  const randStdNormal = Math.sqrt(-2.0 * Math.log(u1)) * Math.sign(2.0 * Math.PI * u2);
+  return mean + stdDev * randStdNormal;
 }
