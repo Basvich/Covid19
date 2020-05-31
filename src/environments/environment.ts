af29b731f0c6b06d21be63807baf8dfb7c0fceee
Basilio <basilio.alvarez@gmail.com>
2020-05-31 22:54:49 +0200

    build V1.4

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,7 @@
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 declare var require: any;
+
 export const environment = {
   appVersion: require('../../package.json').version,
   production: false
